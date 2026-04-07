ff487382074edace6d2a301916a7d22ee7ea7a63
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-07 15:49:56 -0500

    Print goodbye

--- src/main/java/com/pluralsight/HelloWord.java
+++ src/main/java/com/pluralsight/HelloWord.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Craig!");
+        System.out.println("Goodbye Craig.");
     }
 }
